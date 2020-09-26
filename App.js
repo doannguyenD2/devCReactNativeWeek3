d5f7d71842e15a2138ff4cc613ab88d8be9c4f8f
doannguyenD2 <40143494+doannguyenD2@users.noreply.github.com>
2020-09-26 22:47:29 +0700

    init

--- App.js
+++ App.js
@@ -1,13 +1,93 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const [gamePrompt, setGamePrompt] = useState('Fire!');
+
   return (
     <View style={styles.container}>
+      <Text>{gamePrompt}</Text>
+      <TouchableOpacity style={styles.buttonStyle}>
+        <Text style={styles.buttonText}>Rock</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#e9ebee'
+  },
+  buttonContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonStyle: {
+    width: 200,
+    margin: 10,
+    height: 50,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#640D14',
+  },
+  buttonText: {
+    fontSize: 25,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  choicesContainer: {
+    margin: 10,
+    borderWidth: 2,
+    paddingTop: 100,
+    shadowRadius: 5,
+    paddingBottom: 100,
+    borderColor: 'grey',
+    shadowOpacity: 0.90,
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    justifyContent: 'space-around',
+    shadowColor: 'rgba(0,0,0,0.2)',
+    shadowOffset: { height: 5, width: 5 },
+  },
+  choiceContainer: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  choiceDescription: {
+    fontSize: 25,
+    color: '#250902',
+    fontWeight: 'bold',
+    textDecorationLine: 'underline'
+  },
+  choiceCardTitle: {
+    fontSize: 30,
+    color: '#250902'
+  },
+  choiceImage: {
+    width: 150,
+    height: 150,
+    padding: 10,
+  }
 });
+const CHOICES = [
+  {
+    name: 'rock',
+    uri: ''
+  },
+  {
+    name: 'paper',
+    uri: ''
+  },
+  {
+    name: 'scissors',
+    uri:
+      ''
+  }
+];
