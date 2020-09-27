2b9ca662bb0d6deeb2d9c49349abd546cc4fd7ab
doannguyenD2 <40143494+doannguyenD2@users.noreply.github.com>
2020-09-27 18:12:42 +0700

    basic game

--- App.js
+++ App.js
@@ -1,17 +1,90 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import { TouchableOpacity } from 'react-native';
 
 export default function App() {
   const [gamePrompt, setGamePrompt] = useState('Fire!');
+  const [userChoice, setUserChoice] = useState({});
+  const [computerChoice, setComputerChoice] = useState({});
+  const Button = props => (
+    <TouchableOpacity
+      style={styles.buttonStyle}
+      onPress={() => props.onPress(props.name)}
+    >
+      <Text style={styles.buttonText}>
+        {props.name.charAt(0).toUpperCase() + props.name.slice(1)}
+      </Text>
+    </TouchableOpacity>
+  );
+  const ChoiceCard = ({ player, choice: { uri, name } }) => {
+    const title = name && name.charAt(0).toUpperCase() + name.slice(1);
+
+    return (
+      <View style={styles.choiceContainer}>
+        <Text style={styles.choiceDescription}>{player}</Text>
+        <Image source={{ uri }} resizeMode="contain" style={styles.choiceImage} />
+        <Text style={styles.choiceCardTitle}>{title}</Text>
+      </View>
+    );
+  };
+  // const onPress = userChoice => {
+  //   console.log('userChoice', userChoice);
+  // };
+  const onPress = playerChoice => {
+    const [result, compChoice] = getRoundOutcome(playerChoice);
+
+    const newUserChoice = CHOICES.find(choice => choice.name === playerChoice);
+    const newComputerChoice = CHOICES.find(choice => choice.name === compChoice);
+
+    setGamePrompt(result);
+    setUserChoice(newUserChoice);
+    setComputerChoice(newComputerChoice);
+  };
+  const getRoundOutcome = userChoice => {
+    const computerChoice = randomComputerChoice().name;
+    let result;
+
+    if (userChoice === 'rock') {
+      result = computerChoice === 'scissors' ? 'Victory!' : 'Defeat!';
+    }
+    if (userChoice === 'paper') {
+      result = computerChoice === 'rock' ? 'Victory!' : 'Defeat!';
+    }
+    if (userChoice === 'scissors') {
+      result = computerChoice === 'paper' ? 'Victory!' : 'Defeat!';
+    }
+
+    if (userChoice === computerChoice) result = 'Tie game!';
+    return [result, computerChoice];
+  };
+  const randomComputerChoice = () =>
+    CHOICES[Math.floor(Math.random() * CHOICES.length)];
+  const getResultColor = () => {
+    if (gamePrompt === 'Victory!') return 'green';
+    if (gamePrompt === 'Defeat!') return 'red';
+    return null;
+  };
 
   return (
     <View style={styles.container}>
-      <Text>{gamePrompt}</Text>
-      <TouchableOpacity style={styles.buttonStyle}>
-        <Text style={styles.buttonText}>Rock</Text>
-      </TouchableOpacity>
+    <Text style={{ fontSize: 35, color: getResultColor() }}>{gamePrompt}</Text>
+    <View style={styles.choicesContainer}>
+        <ChoiceCard
+          player="Player"
+          choice={userChoice}
+        />
+        <Text style={{ color: '#250902' }}>vs</Text>
+        <ChoiceCard
+          player="Computer"
+          choice={computerChoice}
+        />
+      </View>
+      {
+        CHOICES.map(choice => {
+          return <Button key={choice.name} name={choice.name} onPress={onPress} />;
+        })
+      }
     </View>
   );
 }
@@ -79,15 +152,14 @@ const styles = StyleSheet.create({
 const CHOICES = [
   {
     name: 'rock',
-    uri: ''
+    uri: require('./assets/rock.png')
   },
   {
     name: 'paper',
-    uri: ''
+    uri: require('./assets/paper.png')
   },
   {
     name: 'scissors',
-    uri:
-      ''
+    uri: require('./assets/scissor.png')
   }
 ];
